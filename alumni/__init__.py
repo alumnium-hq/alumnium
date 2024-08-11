e816ce9fd0fa1c6609fbd0a3c4940bcf33ed002b
Alex Rodionov <p0deje@gmail.com>
2024-08-11 11:48:09 -0700

    fix: Google test
    The problem is that Google page now shows a "Sign In" popup that takes a
    focus and `submit()` fails to trigger the search.
    The prompt-shot is click the field now to force focus.

--- alumni/__init__.py
+++ alumni/__init__.py
@@ -51,6 +51,10 @@ Webpage ARIA tree:
                 AIMessage(
                     """
 [
+    click({
+        "aria_role": "textbox",
+        "aria_name": "New Todo Input"
+    }),
     type({
         "text": "Hello",
         "aria_role": "textbox",
