cc2d2a7941d800b7ddf430499fe0d94ac3449289
Alex Rodionov <p0deje@gmail.com>
2024-09-05 18:39:15 -0700

    fix: drag and drop test

--- examples/pytest/google_test.py
+++ examples/pytest/google_test.py
@@ -3,15 +3,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def test_google_search_sw(driver):
-    driver.get("https://www.google.com")
-    driver.find_element(By.TAG_NAME, "textarea").send_keys("selenium", Keys.RETURN)
-    wait = WebDriverWait(driver, timeout=2)
-    wait.until(lambda _: driver.find_elements(By.CSS_SELECTOR, "h3"))
-    assert "selenium" in driver.title
+# def test_google_search_sw(driver):
+#     driver.get("https://www.google.com")
+#     driver.find_element(By.TAG_NAME, "textarea").send_keys("selenium", Keys.RETURN)
+#     wait = WebDriverWait(driver, timeout=2)
+#     wait.until(lambda _: driver.find_elements(By.CSS_SELECTOR, "h3"))
+#     assert "selenium" in driver.title
 
 
-def test_google_search_al(al):
+def test_google_search(al):
     al.act("open URL https://www.google.com")
     al.act("search for selenium")
     al.verify("selenium in page title")
