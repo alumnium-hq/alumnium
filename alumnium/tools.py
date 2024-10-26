67c795b25f14e0cdb4a1572b6e864ee93c5ba7e5
Alex Rodionov <p0deje@gmail.com>
2024-10-25 18:15:45 -0700

    feat: use Selenium for interactions and add `SelectTool`

--- alumnium/tools.py
+++ alumnium/tools.py
@@ -1,20 +1,34 @@
 from pydantic import BaseModel, Field
 
-from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
-class Coordinates(BaseModel):
-    x: float
-    y: float
-
-    @classmethod
-    def for_element(cls, id: int, driver: WebDriver):
-        box = driver.execute_cdp_cmd("DOM.getBoxModel", {"backendNodeId": id})["model"]["border"]
-        return cls(
-            x=(box[0] + box[4]) / 2,
-            y=(box[1] + box[5]) / 2,
-        )
+def _find_element(backend_node_id: int, driver: WebDriver):
+    driver.execute_cdp_cmd("DOM.enable", {})
+    driver.execute_cdp_cmd("DOM.getFlattenedDocument", {})
+    node_ids = driver.execute_cdp_cmd("DOM.pushNodesByBackendIdsToFrontend", {"backendNodeIds": [backend_node_id]})
+    node_id = node_ids["nodeIds"][0]
+    driver.execute_cdp_cmd(
+        "DOM.setAttributeValue",
+        {
+            "nodeId": node_id,
+            "name": "data-alumnium-id",
+            "value": str(backend_node_id),
+        },
+    )
+    element = driver.find_element(By.CSS_SELECTOR, f"[data-alumnium-id='{backend_node_id}']")
+    driver.execute_cdp_cmd(
+        "DOM.removeAttribute",
+        {
+            "nodeId": node_id,
+            "name": "data-alumnium-id",
+        },
+    )
+    return element
 
 
 class ClickTool(BaseModel):
@@ -23,16 +37,7 @@ class ClickTool(BaseModel):
     id: int = Field(description="Element identifier (ID)")
 
     def invoke(self, driver: WebDriver):
-        driver.execute_cdp_cmd("DOM.scrollIntoViewIfNeeded", {"backendNodeId": self.id})
-        point = Coordinates.for_element(self.id, driver)
-        driver.execute_cdp_cmd(
-            "Input.dispatchMouseEvent",
-            {"type": "mousePressed", "x": point.x, "y": point.y, "button": "left", "clickCount": 1},
-        )
-        driver.execute_cdp_cmd(
-            "Input.dispatchMouseEvent",
-            {"type": "mouseReleased", "x": point.x, "y": point.y, "button": "left", "clickCount": 1},
-        )
+        _find_element(self.id, driver).click()
 
 
 class DragAndDropTool(BaseModel):
@@ -42,23 +47,8 @@ class DragAndDropTool(BaseModel):
     to_id: int = Field(description="Identifier (ID) of element to drop onto")
 
     def invoke(self, driver: WebDriver):
-        driver.execute_cdp_cmd("DOM.scrollIntoViewIfNeeded", {"backendNodeId": self.id})
-
-        drag = Coordinates.for_element(self.from_id, driver)
-        drop = Coordinates.for_element(self.to_id, driver)
-
-        driver.execute_cdp_cmd(
-            "Input.dispatchMouseEvent",
-            {"type": "mousePressed", "x": drag.x, "y": drag.y, "button": "left", "clickCount": 1},
-        )
-        driver.execute_cdp_cmd(
-            "Input.dispatchMouseEvent",
-            {"type": "mouseMoved", "x": drop.x, "y": drop.y, "button": "left", "clickCount": 1},
-        )
-        driver.execute_cdp_cmd(
-            "Input.dispatchMouseEvent",
-            {"type": "mouseReleased", "x": drop.x, "y": drop.y, "button": "left", "clickCount": 1},
-        )
+        actions = ActionChains(driver)
+        actions.drag_and_drop(_find_element(self.from_id, driver), _find_element(self.to_id, driver)).perform()
 
 
 class HoverTool(BaseModel):
@@ -67,11 +57,18 @@ class HoverTool(BaseModel):
     id: int = Field(description="Element identifier (ID)")
 
     def invoke(self, driver: WebDriver):
-        driver.execute_cdp_cmd("DOM.scrollIntoViewIfNeeded", {"backendNodeId": self.id})
-        point = Coordinates.for_element(self.id, driver)
-        driver.execute_cdp_cmd(
-            "Input.dispatchMouseEvent", {"type": "mouseMoved", "x": point.x, "y": point.y, "button": "left"}
-        )
+        actions = ActionChains(driver)
+        actions.move_to_element(_find_element(self.id, driver)).perform()
+
+
+class SelectTool(BaseModel):
+    """Selects an option in a dropdown."""
+
+    id: int = Field(description="Element identifier (ID)")
+    option: str = Field(description="Option to select.")
+
+    def invoke(self, driver: WebDriver):
+        Select(_find_element(self.id, driver)).select_by_visible_text(self.option)
 
 
 class TypeTool(BaseModel):
@@ -86,9 +83,7 @@ class TypeTool(BaseModel):
         if self.submit:
             input.append(Keys.RETURN)
 
-        driver.execute_cdp_cmd("DOM.scrollIntoViewIfNeeded", {"backendNodeId": self.id})
-        driver.execute_cdp_cmd("DOM.focus", {"backendNodeId": self.id})
-        element = driver.switch_to.active_element
+        element = _find_element(self.id, driver)
         element.clear()
         element.send_keys(*input)
 
@@ -97,5 +92,6 @@ ALL_TOOLS = {
     "ClickTool": ClickTool,
     "DragAndDropTool": DragAndDropTool,
     "HoverTool": HoverTool,
+    "SelectTool": SelectTool,
     "TypeTool": TypeTool,
 }
