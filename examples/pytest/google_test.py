961d35a955a7e2a393e48d0f1e425020da1008c0
Alex Rodionov <p0deje@gmail.com>
2024-11-03 05:29:09 -0800

    fix: explict instructions in google test

--- examples/pytest/google_test.py
+++ examples/pytest/google_test.py
@@ -13,6 +13,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def test_google_search(al, driver):
     driver.get("https://www.google.com")
-    al.do("search for selenium")
+    al.do("type selenium to search and press enter")
     al.check("selenium in page title")
     al.check("selenium.dev is present in the search results")
