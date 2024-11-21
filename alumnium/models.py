6ef1325b321454f9f6f7e1d87132fdf1f907826c
Alex Rodionov <p0deje@gmail.com>
2024-11-20 19:04:08 -0800

    feat: llama-3.2 on bedrock
    This is initial support for Llama 3.2 90B vision instruct model!
    
    For such a big model, it's very hard to make it work locally with all
    Alumnium requirements (tool calling, structured output, multimodal). For
    the time being, AWS Bedrock is a provider that proves to work fine in
    this initial implementation.
    
    There are few things to keep in mind in this initial implementation:
    1. tool calling types are less strict (e.g. it's common for the model to
       return str instead of int/bool). Pydantic coercion helps with this.
    2. vision is disabled for now - when the model is used both with image
       and structured output, the latter does not work. This can probably be
       worked around with custom response parsing, but this is left for the
       future (maybe AWS will fix it eventually).
    3. images needs to be resized to max of 1120x1120, but this is not
       implemented yet due to the previous point.
    
    It would be great to use Ollama or Llama.cpp to support true local
    inference. This commit however proves that Alumnium can be used with
    open models!

--- alumnium/models.py
+++ alumnium/models.py
@@ -6,6 +6,7 @@ class Model(Enum):
     AZURE_OPENAI = "gpt-4o-mini"  # 2024-07-18
     ANTHROPIC = "claude-3-haiku-20240307"
     AWS_ANTHROPIC = "anthropic.claude-3-haiku-20240307-v1:0"
+    AWS_META = "us.meta.llama3-2-90b-instruct-v1:0"
     GOOGLE = "gemini-1.5-flash-002"
     OPENAI = "gpt-4o-mini-2024-07-18"
 
--- examples/pytest/calculator_test.py
+++ examples/pytest/calculator_test.py
@@ -3,6 +3,7 @@ from pytest import mark, raises
 
 
 @mark.xfail(Model.load() == Model.AWS_ANTHROPIC, reason="Bedrock version of Haiku is subpar")
+@mark.xfail(Model.load() == Model.AWS_META, reason="It is too hard for Llama 3.2")
 def test_addition(al, driver):
     driver.get("https://seleniumbase.io/apps/calculator")
     al.do("1 + 1 =")
