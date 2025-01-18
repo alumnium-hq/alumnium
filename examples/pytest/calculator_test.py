7e36829da9cd19d200e4fd51097503f82998dfd1
Alex Rodionov <p0deje@gmail.com>
2025-01-17 19:26:06 -0800

    test: adapt to playwright

--- examples/pytest/calculator_test.py
+++ examples/pytest/calculator_test.py
@@ -2,27 +2,27 @@ from alumnium import Model
 from pytest import mark, raises, hookimpl
 
 
-def test_addition(al, driver):
-    driver.get("https://seleniumbase.io/apps/calculator")
+def test_addition(al, navigate):
+    navigate("https://seleniumbase.io/apps/calculator")
     al.do("2 + 2 =")
     al.check("calculator result is 4")
     with raises(AssertionError):
         al.check("calculator result is 3")
 
 
-def test_subtraction(al, driver):
-    driver.get("https://seleniumbase.io/apps/calculator")
+def test_subtraction(al, navigate):
+    navigate("https://seleniumbase.io/apps/calculator")
     al.do("5 - 3 =")
     al.check("calculator result is 2")
 
 
-def test_multiplication(al, driver):
-    driver.get("https://seleniumbase.io/apps/calculator")
+def test_multiplication(al, navigate):
+    navigate("https://seleniumbase.io/apps/calculator")
     al.do("3 * 4 =")
     al.check("calculator result is 12")
 
 
-def test_division(al, driver):
-    driver.get("https://seleniumbase.io/apps/calculator")
+def test_division(al, navigate):
+    navigate("https://seleniumbase.io/apps/calculator")
     al.do("8 / 2 =")
     al.check("calculator result is 4")
