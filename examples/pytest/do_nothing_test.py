e59bd999d441010b7c6050eede2075d188c36cee
Alex Rodionov <p0deje@gmail.com>
2025-01-17 19:26:06 -0800

    test: adapt to playwright

--- examples/pytest/do_nothing_test.py
+++ examples/pytest/do_nothing_test.py
@@ -1,5 +1,5 @@
-def test_doing_nothing(al, driver):
-    driver.get("https://the-internet.herokuapp.com/login")
+def test_doing_nothing(al, navigate):
+    navigate("https://the-internet.herokuapp.com/login")
     # This should not attempt to do anything on the page
     # because there are no relevant controls.
     al.do("select USA in Country")
--- examples/pytest/google_test.py
+++ examples/pytest/google_test.py
@@ -1,5 +1,5 @@
 def test_google_search(al, navigate):
-    navigate("https://www.google.com")
+    navigate("https://www.duckduckgo.com")  # TODO: captcha
     al.do("search for selenium")
     al.check("selenium in page title")
     al.check("selenium.dev is present in the search results")
