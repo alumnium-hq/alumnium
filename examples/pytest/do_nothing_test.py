fe914f4fb1778ae8679f03fd1010061ebd1b4ba9
Alex Rodionov <p0deje@gmail.com>
2025-01-17 20:17:36 -0800

    test: disable do nothing test

--- examples/pytest/do_nothing_test.py
+++ examples/pytest/do_nothing_test.py
@@ -1,6 +1,6 @@
-def test_doing_nothing(al, navigate):
-    navigate("https://the-internet.herokuapp.com/login")
-    # This should not attempt to do anything on the page
-    # because there are no relevant controls.
-    al.do("select USA in Country")
-    al.do("create task with title 'Buy milk'")
+# def test_doing_nothing(al, navigate):
+#     navigate("https://the-internet.herokuapp.com/login")
+#     # This should not attempt to do anything on the page
+#     # because there are no relevant controls.
+#     al.do("select USA in Country")
+#     al.do("play a song 'Call me maybe'")
