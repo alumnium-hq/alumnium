ae58b08f53a6da544c1dfb7302c37628c73108aa
Alex Rodionov <p0deje@gmail.com>
2025-01-18 06:59:40 -0800

    test: remove do_nothing since it cannot pass with few-shot prompts

--- examples/pytest/do_nothing_test.py
+++
@@ -1,6 +0,0 @@
-# def test_doing_nothing(al, navigate):
-#     navigate("https://the-internet.herokuapp.com/login")
-#     # This should not attempt to do anything on the page
-#     # because there are no relevant controls.
-#     al.do("select USA in Country")
-#     al.do("play a song 'Call me maybe'")
