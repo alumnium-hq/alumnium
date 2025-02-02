177d32b5e6fb7ddf6516b0a22f686ec4ac8a7e5c
Alex Rodionov <p0deje@gmail.com>
2025-02-02 10:18:38 -0800

    test: improve language of drag&drop test

--- examples/pytest/drag_and_drop_test.py
+++ examples/pytest/drag_and_drop_test.py
@@ -5,6 +5,6 @@ from pytest import mark
 @mark.xfail(Model.load() == Model.AWS_META, reason="https://github.com/boto/boto3/issues/4374")
 def test_drag_and_drop(al, navigate):
     navigate("https://the-internet.herokuapp.com/drag_and_drop")
-    assert al.get("square titles ordered from left to right", vision=True) == ["A", "B"]
+    assert al.get("titles of squares ordered from left to right", vision=True) == ["A", "B"]
     al.do("move square A to square B")
-    assert al.get("square titles ordered from left to right", vision=True) == ["B", "A"]
+    assert al.get("titles of squares ordered from left to right", vision=True) == ["B", "A"]
