670b40fdb515d733f916cc9f3287c884032f5369
Alex Rodionov <p0deje@gmail.com>
2025-02-04 17:59:29 -0800

    refactor!: remove `ALUMNIUM_DEBUG`
    `ALUMNIUM_LOG_LEVEL=debug` should be used instead

--- alumnium/__init__.py
+++ alumnium/__init__.py
@@ -2,16 +2,11 @@ import logging
 from os import getenv
 from sys import stdout
 
-
 logger = logging.getLogger(__name__)
 
 level = getenv("ALUMNIUM_LOG_LEVEL", None)
-if getenv("ALUMNIUM_DEBUG", "0") == "1":
-    level = "debug"
-
 if level:
     logger.setLevel(level.upper())
     logger.addHandler(logging.StreamHandler(stdout))
-
 
 from .alumni import *
