37830796a82fd37f68f3cf56e1fd9f3b7162e282
Alex Rodionov <p0deje@gmail.com>
2025-02-11 20:03:46 -0800

    revert: accidental test deletion

--- examples/pytest/calculator_test.py
+++ examples/pytest/calculator_test.py
@@ -2,3 +2,21 @@ def test_addition(al, navigate):
     navigate("https://seleniumbase.io/apps/calculator")
     al.do("2 + 2 =")
     assert al.get("calculator value") == 4
+
+
+def test_subtraction(al, navigate):
+    navigate("https://seleniumbase.io/apps/calculator")
+    al.do("5 - 3 =")
+    assert al.get("calculator value") == 2
+
+
+def test_multiplication(al, navigate):
+    navigate("https://seleniumbase.io/apps/calculator")
+    al.do("3 * 4 =")
+    assert al.get("calculator value") == 12
+
+
+def test_division(al, navigate):
+    navigate("https://seleniumbase.io/apps/calculator")
+    al.do("8 / 2 =")
+    assert al.get("calculator value") == 4
