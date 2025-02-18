8dea149c0faa4240edf9d78ebab78689dfb6bb55
Alex Rodionov <p0deje@gmail.com>
2025-02-17 17:18:38 -0800

    test: add per-test examples

--- examples/pytest/search_test.py
+++ examples/pytest/search_test.py
@@ -1,4 +1,22 @@
-from pytest import raises
+from pytest import fixture, raises
+
+from alumnium import Model
+
+
+@fixture(autouse=True)
+def learn(al):
+    # Gemini/Haiku have issues learning how to search
+    if Model.load() in [Model.AWS_META, Model.GOOGLE]:
+        al.learn(
+            goal="search for artificial intelligence",
+            actions=[
+                "type 'artificial intelligence' into a search field",
+                "press key 'Enter'",
+            ],
+        )
+    yield
+    if Model.load() in [Model.AWS_META, Model.GOOGLE]:
+        al.planner_agent.prompt_with_examples.examples.clear()
 
 
 def test_search(al, navigate):
