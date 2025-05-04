715611cf8178581ce78029b139415d912c4b09ec
Alex Rodionov <p0deje@gmail.com>
2025-05-03 19:28:18 -0700

    feat: support mistral-small-31:24b on ollama
    This is the very first fully local model support.
    Mistral Small 3.1 24B was chosen because it is a great small model that
    supports both vision and tool calling on Ollama.
    
    In testing on my MacBook with M1 Pro / 32GB  RAM, it passes examples,
    though it is very slow:
    
    ```
    $ ALUMNIUM_MODEL=ollama poetry run behave
    ...
    1 feature passed, 0 failed, 0 skipped
    8 scenarios passed, 0 failed, 0 skipped
    49 steps passed, 0 failed, 0 skipped, 0 undefined
    Took 49m49.574s
    ````
    
    It's not being tested on CI yet because GHA runners cannot run the model.
    We'll need to add proper testing later. We can potentially test model
    capabilities by using the Mistral AI platform, but that could be different
    from the Ollama implementation. Another option is to find a cloud-based
    solution that can run the model with Ollama and expose an API.

--- alumnium/alumni.py
+++ alumnium/alumni.py
@@ -5,6 +5,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_aws import ChatBedrockConverse
 from langchain_deepseek import ChatDeepSeek
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
 from langchain_openai import AzureChatOpenAI, ChatOpenAI
 from playwright.sync_api import Page
 from retry import retry
@@ -50,6 +51,8 @@ class Alumni:
             llm = ChatDeepSeek(model=self.model.value, temperature=0)
         elif self.model == Model.GOOGLE:
             llm = ChatGoogleGenerativeAI(model=self.model.value, temperature=0)
+        elif self.model == Model.OLLAMA:
+            llm = ChatOllama(model=self.model.value, temperature=0)
         elif self.model == Model.OPENAI:
             llm = ChatOpenAI(model=self.model.value, temperature=0, seed=1)
         else:
