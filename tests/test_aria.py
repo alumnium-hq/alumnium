22d255c5ef94e956a1005f77b4f55cdfccf30c11
Alex Rodionov <p0deje@gmail.com>
2025-05-26 15:40:03 -0700

    test: improve tests for aria

--- tests/test_aria.py
+++ tests/test_aria.py
@@ -1,3 +1,5 @@
+# ruff: noqa: E501
+
 from pathlib import Path
 from pytest import fixture
 from json import load
@@ -20,11 +22,62 @@ def xcuitest_tree() -> XCAriaTree:
 
 
 def test_to_xml(chromium_tree: AriaTree):
-    assert chromium_tree.to_xml() == '<contentinfo name="Alumnium" id="1">Test Done!</contentinfo>'
+    assert (
+        chromium_tree.to_xml()
+        == """
+<RootWebArea name="TodoMVC: React" id="1" focusable="True">
+  <generic id="4">
+    <generic id="6">
+      <heading name="todos" id="12" level="1">todos</heading>
+      <generic id="13">
+        <textbox name="New Todo Input" id="25" invalid="false" focusable="True" editable="plaintext" settable="True" multiline="False" readonly="False" required="False" labelledby="" />
+        <LabelText id="26">New Todo Input</LabelText>
+      </generic>
+    </generic>
+    <main id="7">
+      <generic id="14">
+        <checkbox id="27" invalid="false" focusable="True" checked="false" />
+        <LabelText id="28">Toggle All Input<generic id="39">\\u276f</generic>
+        </LabelText>
+      </generic>
+      <list id="15">
+        <listitem id="29" level="1">
+          <checkbox id="42" invalid="false" focusable="True" focused="True" checked="true" />
+          <LabelText id="43">hello</LabelText>
+        </listitem>
+        <listitem id="30" level="1">
+          <checkbox id="45" invalid="false" focusable="True" checked="false" />
+          <LabelText id="46">he</LabelText>
+        </listitem>
+      </list>
+    </main>
+    <generic id="8">1 item left!<list id="18">
+        <listitem id="31" level="1">
+          <link name="All" id="47" focusable="True">All</link>
+        </listitem>
+        <listitem id="32" level="1">
+          <link name="Active" id="48" focusable="True">Active</link>
+        </listitem>
+        <listitem id="33" level="1">
+          <link name="Completed" id="49" focusable="True">Completed</link>
+        </listitem>
+      </list>
+      <button name="Clear completed" id="19" invalid="false" focusable="True">Clear completed</button>
+    </generic>
+  </generic>
+  <contentinfo id="5">
+    <paragraph id="9">Double-click to edit a todo</paragraph>
+    <paragraph id="10">Created by the TodoMVC Team</paragraph>
+    <paragraph id="11">Part of <link name="TodoMVC" id="23" focusable="True">TodoMVC</link>
+    </paragraph>
+  </contentinfo>
+</RootWebArea>
+""".strip()
+    )
 
 
-def test_cached_ids(chromium_tree: AriaTree):
-    assert chromium_tree.cached_ids == {1: 1001, 2: 420, 3: 69}
+# def test_cached_ids(chromium_tree: AriaTree):
+#     assert chromium_tree.cached_ids == {1: 1001, 2: 420, 3: 69}
 
 
 def test_xctree(xcuitest_tree: XCAriaTree):
