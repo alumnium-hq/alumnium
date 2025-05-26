494e2bb2ef83e25ede6c7eedd3db3ef662563976
Alex Rodionov <p0deje@gmail.com>
2025-05-26 15:31:43 -0700

    test: add base xctree test

--- tests/test_aria.py
+++ tests/test_aria.py
@@ -1,93 +1,60 @@
-import pytest
+from pathlib import Path
+from pytest import fixture
+from json import load
 
 from alumnium.aria import AriaTree, XCAriaTree
 
 
-@pytest.fixture
-def tree() -> AriaTree:
-    return AriaTree(
-        {
-            "nodes": [
-                {
-                    "backendDOMNodeId": 1001,
-                    "childIds": ["420", "69"],
-                    "chromeRole": {"type": "internalRole", "value": 111},
-                    "ignored": False,
-                    "name": {
-                        "sources": [
-                            {
-                                "type": "contents",
-                                "value": {"type": "computedString", "value": "Alumnium"},
-                            }
-                        ],
-                        "type": "computedString",
-                        "value": "Alumnium",
-                    },
-                    "nodeId": "1001",
-                    "role": {"type": "role", "value": "contentinfo"},
-                },
-                {
-                    "backendDOMNodeId": 420,
-                    "chromeRole": {"type": "internalRole", "value": 212},
-                    "ignored": True,
-                    "ignoredReasons": [
-                        {
-                            "name": "uninteresting",
-                            "value": {"type": "boolean", "value": True},
-                        }
-                    ],
-                    "name": {
-                        "sources": [
-                            {"attribute": "aria-label", "type": "attribute"},
-                            {"attribute": "title", "type": "attribute"},
-                        ],
-                        "type": "computedString",
-                        "value": "",
-                    },
-                    "nodeId": "420",
-                    "parentId": "1001",
-                    "properties": [],
-                    "role": {"type": "role", "value": "generic"},
-                },
-                {
-                    "backendDOMNodeId": 69,
-                    "childIds": [],
-                    "chromeRole": {"type": "internalRole", "value": 169},
-                    "ignored": False,
-                    "name": {
-                        "sources": [
-                            {
-                                "type": "contents",
-                                "value": {
-                                    "type": "computedString",
-                                    "value": "Test Done!",
-                                },
-                            }
-                        ],
-                        "type": "computedString",
-                        "value": "Test Done!",
-                    },
-                    "nodeId": "69",
-                    "parentId": "1001",
-                    "properties": [],
-                    "role": {"type": "internalRole", "value": "StaticText"},
-                },
-            ]
-        }
+@fixture
+def chromium_tree() -> AriaTree:
+    with open(Path(__file__).parent / "fixtures/chromium_accessibility_tree.json", "r") as f:
+        json = load(f)
+    return AriaTree(json)
+
+
+@fixture
+def xcuitest_tree() -> XCAriaTree:
+    with open(Path(__file__).parent / "fixtures/xcuitest_accessibility_tree.xml", "r") as f:
+        xml = f.read()
+    return XCAriaTree(xml)
+
+
+def test_to_xml(chromium_tree: AriaTree):
+    assert chromium_tree.to_xml() == '<contentinfo name="Alumnium" id="1">Test Done!</contentinfo>'
+
+
+def test_cached_ids(chromium_tree: AriaTree):
+    assert chromium_tree.cached_ids == {1: 1001, 2: 420, 3: 69}
+
+
+def test_xctree(xcuitest_tree: XCAriaTree):
+    assert (
+        xcuitest_tree.to_xml()
+        == """
+<Application name="ToDoList" id="1">
+  <Window id="2">
+    <generic id="61">
+      <generic id="62">
+        <generic id="65">
+          <generic id="66">
+            <generic id="67">
+              <StaticText id="68">Welcome to ToDoList</StaticText>
+            </generic>
+            <Image name="roundedIcon" id="69" />
+            <StaticText id="70">Start with a quick onboarding</StaticText>
+            <generic id="71">
+              <generic id="72">
+                <Button name="Continue" id="73">
+                  <StaticText id="74">Continue</StaticText>
+                  <Image name="checkmark.circle" id="75" />
+                </Button>
+              </generic>
+            </generic>
+          </generic>
+        </generic>
+      </generic>
+    </generic>
+  </Window>
+</Application>
+    """.strip()
     )
-
-
-def test_to_xml(tree: AriaTree):
-    assert tree.to_xml() == '<contentinfo name="Alumnium" id="1">Test Done!</contentinfo>'
-
-
-def test_cached_ids(tree: AriaTree):
-    assert tree.cached_ids == {1: 1001, 2: 420, 3: 69}
-
-
-def test_xctree(capsys):
-    # with capsys.disabled():
-    print(XCAriaTree(open("appium.xml").read()).to_xml())
-    import pdb
-
-    pdb.set_trace()
