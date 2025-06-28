669e8d4371a8c369a1dc13b02d0e326fc7d4eaa1
Alex Rodionov <p0deje@gmail.com>
2025-06-28 09:34:57 -0700

    fix: appium URL on CI

--- examples/pytest/conftest.py
+++ examples/pytest/conftest.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from os import getenv
 
-from appium import webdriver as AppiumWebdriver
 from appium.options.ios import XCUITestOptions
-from appium.webdriver.appium_service import AppiumService
+from appium.webdriver.webdriver import WebDriver as Appium
 from playwright.sync_api import Page, sync_playwright
 from pytest import fixture, hookimpl
 from selenium.webdriver import Chrome
@@ -22,19 +21,16 @@ def driver():
         driver = Chrome()
         yield driver
     elif driver == "appium":
-        options = XCUITestOptions().load_capabilities(
-            {
-                "platformName": "iOS",
-                "platformVersion": "18.3",
-                "deviceName": "iPhone 14 Pro",
-                "automationName": "XCUITest",
-                "bundleId": "com.apple.mobilesafari",
-                "noReset": True,
-                "newCommandTimeout": 60000,
-                "showXcodeLog": True,
-            }
-        )
-        driver = AppiumWebdriver.Remote(command_executor="http://127.0.0.1:4724/wd/hub", options=options)
+        options = XCUITestOptions()
+        # https://github.com/ayodejiayankola/To-Do-App-SwiftUI
+        options.automation_name = "XCUITest"
+        options.bundle_id = "com.apple.mobilesafari"
+        options.device_name = "iPhone 15"
+        options.new_command_timeout = 300
+        options.no_reset = True
+        options.platform_name = "iOS"
+        options.platform_version = "17.4"
+        driver = Appium(command_executor="http://localhost:4723", options=options)
         yield driver
     else:
         raise NotImplementedError(f"Driver {driver} not implemented")
@@ -50,12 +46,10 @@ def al(driver):
 @fixture
 def navigate(driver):
     def __navigate(url):
-        if isinstance(driver, Chrome):
+        if isinstance(driver, (Appium, Chrome)):
             driver.get(url)
         elif isinstance(driver, Page):
             driver.goto(url)
-        elif isinstance(driver, AppiumWebdriver.Remote):
-            driver.get(url)
 
     return __navigate
 
@@ -63,12 +57,10 @@ def navigate(driver):
 @fixture
 def execute_script(driver):
     def __execute_script(script):
-        if isinstance(driver, Chrome):
+        if isinstance(driver, (Appium, Chrome)):
             driver.execute_script(script)
         elif isinstance(driver, Page):
             driver.evaluate(script)
-        elif isinstance(driver, AppiumWebdriver.Remote):
-            driver.execute_script(script)
 
     return __execute_script
 
@@ -85,15 +77,12 @@ def pytest_runtest_makereport(item):
         al = item.funcargs["al"]
         driver = item.funcargs["driver"]
 
-        if isinstance(driver, Chrome):
+        if isinstance(driver, (Appium, Chrome)):
             driver.save_screenshot(f"reports/screenshot-{timestamp}.png")
             url = driver.current_url
         elif isinstance(driver, Page):
             driver.screenshot(path=f"reports/screenshot-{timestamp}.png")
             url = driver.url
-        elif isinstance(driver, AppiumWebdriver.Remote):
-            driver.get_screenshot_as_file(f"reports/screenshot-{timestamp}.png")
-            url = al.driver.url
         extras.append(pytest_html.extras.image(f"screenshot-{timestamp}.png"))
         extras.append(pytest_html.extras.json(al.stats()))
         extras.append(pytest_html.extras.url(url))
