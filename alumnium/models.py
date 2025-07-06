5332c55e6248c6aaa7ea8a326db39086680dc972
balaji <balajigajjala55@gmail.com>
2025-07-06 20:31:14 +0530

    added langchain mistral support

--- alumnium/models.py
+++ alumnium/models.py
@@ -9,6 +9,7 @@ class Provider(Enum):
     AWS_META = "aws_meta"
     DEEPSEEK = "deepseek"
     GOOGLE = "google"
+    MISTRAL_AI = "mistral_ai"
     OLLAMA = "ollama"
     OPENAI = "openai"
 
@@ -21,6 +22,7 @@ class Name:
         Provider.AWS_META: "us.meta.llama3-2-90b-instruct-v1:0",
         Provider.DEEPSEEK: "deepseek-chat",
         Provider.GOOGLE: "gemini-2.0-flash-001",
+        Provider.MISTRAL_AI: "mistral-medium-latest",  # points to mistral-medium-2505
         Provider.OLLAMA: "mistral-small3.1",
         Provider.OPENAI: "gpt-4o-mini-2024-07-18",
     }
