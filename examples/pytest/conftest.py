13b653953c48ca8ba37abc7b488336f5e337fc83
Alex Rodionov <p0deje@gmail.com>
2025-07-18 20:12:54 -0700

    test: fix fixture

--- examples/pytest/conftest.py
+++ examples/pytest/conftest.py
@@ -13,19 +13,12 @@ from alumnium import Alumni
 
 load_dotenv(override=True)
 
-
-@fixture()
-def driver_type():
-    return getenv("ALUMNIUM_DRIVER", "selenium")
-
-
-@fixture()
-def headless():
-    return getenv("ALUMNIUM_PLAYWRIGHT_HEADLESS", "true")
+driver_type = getenv("ALUMNIUM_DRIVER", "selenium")
+headless = getenv("ALUMNIUM_PLAYWRIGHT_HEADLESS", "true")
 
 
 @fixture(scope="session", autouse=True)
-def driver(driver_type, headless):
+def driver():
     if driver_type == "playwright":
         with sync_playwright() as playwright:
             is_headless = headless.lower() == "true"
