f4edec8b4387b07b8b9df1b8e9c2f6916e57589e
Alex Rodionov <p0deje@gmail.com>
2025-07-19 14:45:40 -0700

    test(appium): make calculator_test work

--- examples/pytest/calculator_test.py
+++ examples/pytest/calculator_test.py
@@ -22,22 +22,22 @@ def learn(al):
 def test_addition(al, navigate):
     navigate("https://seleniumbase.io/apps/calculator")
     al.do("2 + 2 =")
-    assert al.get("calculator value") == 4
+    assert al.get("value from textfield") == 4
 
 
 def test_subtraction(al, navigate):
     navigate("https://seleniumbase.io/apps/calculator")
     al.do("5 - 3 =")
-    assert al.get("calculator value") == 2
+    assert al.get("value from textfield") == 2
 
 
 def test_multiplication(al, navigate):
     navigate("https://seleniumbase.io/apps/calculator")
     al.do("3 * 4 =")
-    assert al.get("calculator value") == 12
+    assert al.get("value from textfield") == 12
 
 
 def test_division(al, navigate):
     navigate("https://seleniumbase.io/apps/calculator")
     al.do("8 / 2 =")
-    assert al.get("calculator value") == 4
+    assert al.get("value from textfield") == 4
