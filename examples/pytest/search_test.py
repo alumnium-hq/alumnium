67aa8d30adff3708ee93d042cd122723411efe98
Alex Rodionov <p0deje@gmail.com>
2025-08-08 08:43:39 -0700

    revert(openai): give more weight to examples
    This reverts commit 5d7ed6bdb715ba32933dfa0f4612bef520ae7762.

--- examples/pytest/search_test.py
+++ examples/pytest/search_test.py
@@ -1,30 +1,11 @@
 from os import getenv
 
-from pytest import fixture, mark, raises
+from pytest import mark, raises
 
 from alumnium import Model, Provider
 
 alumnium_driver = getenv("ALUMNIUM_DRIVER", "selenium")
 playwright_headless = getenv("ALUMNIUM_PLAYWRIGHT_HEADLESS", "true")
-
-
-@fixture(autouse=True)
-def learn(al):
-    # Claude/Gemini/Mistral have issues learning how to search
-    if Model.current.provider in [
-        Provider.ANTHROPIC,
-        Provider.AWS_ANTHROPIC,
-        Provider.GOOGLE,
-    ]:
-        al.learn(
-            goal="search for artificial intelligence",
-            actions=[
-                "type 'artificial intelligence' into a search field",
-                "press key 'Enter'",
-            ],
-        )
-    yield
-    al.clear_learn_examples()
 
 
 @mark.skipif(
@@ -35,7 +16,7 @@ def learn(al):
 def test_search(al, navigate):
     navigate("https://www.duckduckgo.com")  # Google forces reCAPTCH, so we use DuckDuckGo
 
-    al.do("search for selenium")
+    al.do("type 'selenium' into the search field, then press 'Enter'")
     if alumnium_driver != "appium":
         # DuckDuckGo doesn't change title on mobile browser
         al.check("page title contains selenium")
