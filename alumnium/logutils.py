f0ac58d7950d1466e601530fdca6c9c2f59f4387
Alex Rodionov <p0deje@gmail.com>
2025-08-17 08:19:14 -0700

    feat(log): file logging of langchain

--- alumnium/logutils.py
+++ alumnium/logutils.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+from langchain_core.callbacks import BaseCallbackHandler, FileCallbackHandler
 from rich.console import Console
 from rich.logging import RichHandler
 from rich.theme import Theme
@@ -69,3 +70,25 @@ def configure_logging() -> logging.Logger:
 def get_logger(name: str | None = None) -> logging.Logger:
     base = logging.getLogger("alumnium")
     return base.getChild(name) if name else base
+
+
+def get_langchain_callback() -> BaseCallbackHandler:
+    if ALUMNIUM_LOG_PATH not in ("stdout", "stderr"):
+        if os.path.dirname(ALUMNIUM_LOG_PATH) != "":
+            os.makedirs(os.path.dirname(ALUMNIUM_LOG_PATH), exist_ok=True)
+        filepath = os.path.splitext(ALUMNIUM_LOG_PATH)[0]
+        return LangChainFileCallbackHandler(filename=f"{filepath}-langchain.log", mode="a")
+    else:
+        return LangChainNullCallbackHandler()
+
+
+class LangChainFileCallbackHandler(FileCallbackHandler):
+    def on_llm_start(self, serialized, prompts, *, run_id, parent_run_id=None, tags=None, metadata=None, **kwargs):
+        self._write(f"\n> [llm/start]({run_id}) {prompts}", end="\n")
+
+    def on_llm_end(self, response, *, run_id, parent_run_id=None, **kwargs):
+        self._write(f"\n< [llm/end]({run_id}) {response}", end="\n")
+
+
+class LangChainNullCallbackHandler(BaseCallbackHandler):
+    pass
