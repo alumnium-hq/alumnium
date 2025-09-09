fadb4017ba8fcc680e164b0df34daa3870de34f2
Alex Rodionov <p0deje@gmail.com>
2025-09-09 10:22:02 -0700

    feat: disable cache with `ALUMNIUM_CACHE=false`

--- alumnium/cache_factory.py
+++ alumnium/cache_factory.py
@@ -1,6 +1,8 @@
 from os import getenv
+from typing import Optional
 
 from .cache.filesystem_cache import FilesystemCache
+from .cache.null_cache import NullCache
 from .cache.sqlite_cache import SQLiteCache
 from .logutils import get_logger
 
@@ -9,8 +11,11 @@ logger = get_logger(__name__)
 
 class CacheFactory:
     @staticmethod
-    def create_cache() -> FilesystemCache | SQLiteCache:
-        cache_provider = getenv("ALUMNI_CACHE_PROVIDER", "filesystem")
+    def create_cache() -> Optional[FilesystemCache | SQLiteCache]:
+        if getenv("ALUMNIUM_CACHE", "true").lower() == "false":
+            return NullCache()
+
+        cache_provider = getenv("ALUMNIUM_CACHE_PROVIDER", "filesystem")
         if cache_provider == "sqlite":
             return SQLiteCache()
         elif cache_provider == "filesystem":
