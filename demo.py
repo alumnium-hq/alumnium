870defb3c23eae66ec5289db090fb5d6d84219cf
Alex Rodionov <p0deje@gmail.com>
2025-09-30 07:16:08 -0700

    chore: support playwright in repl

--- demo.py
+++ demo.py
@@ -1,12 +1,24 @@
+from os import getenv
+
+from playwright.sync_api import sync_playwright
 from selenium.webdriver import Chrome
 
 from alumnium import Alumni
 
-driver = Chrome()
-al = Alumni(driver)
+if getenv("ALUMNIUM_DRIVER", "selenium") == "playwright":
+    playwright = sync_playwright().start()
+    headless = getenv("ALUMNIUM_PLAYWRIGHT_HEADLESS", "true").lower() == "true"
+    page = playwright.chromium.launch(headless=headless).new_page()
+    message = "`page` object to interact with Playwright directly"
+    al = Alumni(page)
+else:
+    driver = Chrome()
+    al = Alumni(driver)
+    message = "`driver` object to interact with Selenium directly"
+
 
 print(
-    """
+    f"""
 Welcome to the Alumnium interactive console!
 
 Use the `al` object to interact with Alumni. For example:
@@ -14,6 +26,6 @@ Use the `al` object to interact with Alumni. For example:
     - al.check("search results contain selenium.dev")
     - al.get("atomic number")
 
-You can also use the `driver` object to interact with the Selenium directly.
+You can also use {message}.
     """
 )
