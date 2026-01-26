5e3662683f0a1268424fbe6e2ca9c29d1996a980
Alex Rodionov <p0deje@gmail.com>
2026-01-26 11:44:32 -0800

    chore(ts): load watier scripts from python

--- packages/typescript/src/drivers/scripts/waitFor.js
+++ packages/typescript/src/drivers/scripts/waitFor.js
@@ -1,8 +1,1 @@
-const done = arguments[arguments.length - 1];
-const args = Array.from(arguments).slice(0, -1);
-const symbol = Symbol.for("alumnium");
-
-window[symbol]
-  .waitForStability(...args)
-  .then(done)
-  .catch((err) => done(err.message));
+../../../../python/src/alumnium/drivers/scripts/waitFor.js
