28f849e29c7c0d5e0c3dffd7f081383a1b68d9f6
Alex Rodionov <p0deje@gmail.com>
2026-02-03 14:44:00 -0800

    test: add example tests for tabs

--- packages/python/examples/pytest/tabs_test.py
+++ packages/python/examples/pytest/tabs_test.py
@@ -1,4 +1,37 @@
-def test_autoswitch_to_tab(al, navigate):
-    navigate("https://the-internet.herokuapp.com/windows")
-    al.do("click on 'Click Here' link")
-    assert al.get("header text") == "New Window"
+from os import getenv
+
+from pytest import mark
+
+from alumnium.alumni import Alumni
+from alumnium.tools import SwitchToNextTabTool, SwitchToPreviousTabTool
+
+
+@mark.xfail(
+    "appium" in getenv("ALUMNIUM_DRIVER", "selenium"),
+    reason="Appium doesn't support tab manipulation yet",
+)
+def test_switching_tabs(al, driver, navigate):
+    al = Alumni(
+        driver,
+        extra_tools=[
+            SwitchToNextTabTool,
+            SwitchToPreviousTabTool,
+        ],
+    )
+
+    navigate("multi_tab_page.html")
+
+    al.do("click on 'Open New Tab' button")
+    assert al.get("current page URL") == "about:blank"
+
+    al.do("switch to previous browser tab")
+    assert al.get("header text") == "Multi-Tab Test Page"
+
+    al.do("switch to next browser tab")
+    assert al.get("current page URL") == "about:blank"
+
+    al.do("switch to next browser tab")
+    assert al.get("header text") == "Multi-Tab Test Page"
+
+    al.do("switch to previous browser tab")
+    assert al.get("current page URL") == "about:blank"
