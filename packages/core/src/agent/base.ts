964540fc0145a099ff8493cea6e4b94331ad1979
Sasha Koss <koss@nocorp.me>
2026-02-17 07:49:51 +0800

    chore(tsify): outline session, session manager and agent classes

---
+++ packages/core/src/agent/base.ts
@@ -0,0 +1,28 @@
+import { AgentUsage } from "./agent.ts";
+
+export class BaseAgent {
+  usage: AgentUsage;
+
+  constructor() {
+    this.usage = { input_tokens: 0, output_tokens: 0, total_tokens: 0 };
+    this.loadPrompts();
+  }
+
+  // TODO:
+  private loadPrompts() {}
+
+  // TODO:
+  protected static shouldRaise() {}
+
+  // TODO:
+  protected invokeChain() {}
+
+  // TODO:
+  private updateUsage() {}
+
+  // TODO:
+  toState() {}
+
+  // TODO:
+  applyState() {}
+}
--- packages/core/src/session/session.ts
+++ packages/core/src/session/session.ts
@@ -1,4 +1,16 @@
+import { LanguageModel } from "ai";
+import { log } from "smollog";
 import { z } from "zod";
+import { ActorAgent } from "../agent/actor.ts";
+import { AreaAgent } from "../agent/area.ts";
+import { ChangesAnalyzerAgent } from "../agent/changesAnalyzer.ts";
+import { LocatorAgent } from "../agent/locator.ts";
+import { PlannerAgent } from "../agent/planner.ts";
+import { RetrieverAgent } from "../agent/retriever.ts";
+import { Model } from "../model/model.ts";
+import { ToolSchema } from "../tool/tool.ts";
+
+// TODO: Find a better place for platform consts and types
 
 export const SESSION_PLATFORMS = [
   "chromium",
@@ -10,10 +22,83 @@ export const SessionPlatform = z.enum(SESSION_PLATFORMS);
 
 export type SessionPlatform = z.infer<typeof SessionPlatform>;
 
-export interface Session {
-  id: string;
-  model: unknown;
+export class Session {
+  sessionId: Session.Id;
+  model: Model;
   platform: SessionPlatform;
+  toolSchemas: ToolSchema[];
   tools: unknown[];
-  llm: unknown;
+  llm: LanguageModel;
+
+  // TODO:
+  #cache: unknown;
+
+  actorAgent: ActorAgent;
+  plannerAgent: PlannerAgent;
+  retrieverAgent: RetrieverAgent;
+  areaAgent: AreaAgent;
+  locatorAgent: LocatorAgent;
+  changesAnalyzerAgent: ChangesAnalyzerAgent;
+
+  constructor(props: Session.ConstructorProps) {
+    const { sessionId, model, platform, toolSchemas, llm } = props;
+    this.sessionId = sessionId;
+    this.model = model;
+    this.platform = platform;
+    this.toolSchemas = toolSchemas;
+    // TODO: Convert toolSchemas to tools
+    this.tools = {} as any;
+
+    // TODO: Create cache
+    this.#cache = {} as any;
+
+    // TODO: Create llm
+    this.llm = {} as any;
+
+    this.actorAgent = new ActorAgent(); // TODO!
+    this.plannerAgent = new PlannerAgent(); // TODO!
+    this.retrieverAgent = new RetrieverAgent(); // TODO!
+    this.areaAgent = new AreaAgent(); // TODO!
+    this.locatorAgent = new LocatorAgent(); // TODO!
+    this.changesAnalyzerAgent = new ChangesAnalyzerAgent(); // TODO!
+
+    log.info(
+      `Created session ${sessionId} with model ${model.provider}/${model.name} and platform ${platform}`,
+    );
+  }
+
+  // TODO:
+  get stats(): unknown {
+    return {} as any;
+  }
+
+  // TODO:
+  processTree() {}
+
+  // TODO:
+  toState() {}
+
+  // TODO:
+  static fromState() {}
+
+  static createId(): Session.Id {
+    return crypto.randomUUID() as Session.Id;
+  }
+}
+
+export namespace Session {
+  export type Id = string & { [idBrand]: true };
+  declare const idBrand: unique symbol;
+
+  export const Id = z.custom<Session.Id>((val) => typeof val === "string", {
+    message: "Invalid session ID",
+  });
+
+  export interface ConstructorProps {
+    sessionId: Id;
+    model: Model;
+    platform: SessionPlatform;
+    toolSchemas: ToolSchema[];
+    llm?: LanguageModel | undefined;
+  }
 }
