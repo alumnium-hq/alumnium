88e0041050bb765a95c2032a1e77552bf47c9e67
Sasha Koss <koss@nocorp.me>
2026-02-18 13:08:44 +0800

    chore(tsify): add failing core accessibility tests

---
+++ packages/core/src/accessibility/chromium.ts
@@ -0,0 +1,12 @@
+// TODO:
+export class ChromiumAccessibilityTree {
+  // TODO:
+  constructor(json: string) {}
+
+  // TODO:
+  elementById(id: number): any {
+    return {
+      backend_node_id: 0,
+    } as any;
+  }
+}
