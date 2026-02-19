bfb9f03eff67b50693ad456a08ecf2ba7e1b046e
Sasha Koss <koss@nocorp.me>
2026-02-19 09:20:36 +0800

    Add dummy server accessibility tree classes

---
+++ packages/core/src/server/accessibility/chromium.ts
@@ -0,0 +1,9 @@
+import { BaseServerAccessibilityTree } from "./base.ts";
+
+// TODO:
+export class ServerChromiumAccessibilityTree extends BaseServerAccessibilityTree {
+  // TODO:
+  constructor(xml: string) {
+    super();
+  }
+}
