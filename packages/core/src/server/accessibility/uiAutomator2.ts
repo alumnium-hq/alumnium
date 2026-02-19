abb94099164cf780d73d0053f6ce3d1cc382987c
Sasha Koss <koss@nocorp.me>
2026-02-19 09:20:36 +0800

    chore(tsify): add dummy server accessibility tree classes

---
+++ packages/core/src/server/accessibility/uiAutomator2.ts
@@ -0,0 +1,9 @@
+import { BaseServerAccessibilityTree } from "./base.ts";
+
+// TODO:
+export class ServerUiAutomator2AccessibilityTree extends BaseServerAccessibilityTree {
+  // TODO:
+  constructor(xml: string) {
+    super();
+  }
+}
---
+++ packages/core/src/server/accessibility/xcuiTest.ts
@@ -0,0 +1,9 @@
+import { BaseServerAccessibilityTree } from "./base.ts";
+
+// TODO:
+export class ServerXcuiTestAccessibilityTree extends BaseServerAccessibilityTree {
+  // TODO:
+  constructor(xml: string) {
+    super();
+  }
+}
