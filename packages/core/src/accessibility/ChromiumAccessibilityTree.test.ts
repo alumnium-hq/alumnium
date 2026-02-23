68abfa5651a6357e341cf6f44d4837c173610f57
Sasha Koss <koss@nocorp.me>
2026-02-23 13:47:25 +0800

    chore(tsify): move accessibility modules from client to core package

--- packages/core/src/accessibility/ChromiumAccessibilityTree.test.ts
+++ packages/core/src/accessibility/ChromiumAccessibilityTree.test.ts
@@ -1,20 +1,20 @@
 import { describe, expect, it } from "bun:test";
 import * as path from "node:path";
-import { ChromiumAccessibilityTree } from "./chromium.ts";
+import { ChromiumAccessibilityTree } from "./ChromiumAccessibilityTree.ts";
 
 const FIXTURE_PATH = path.resolve(
   import.meta.dir,
   "__fixtures__/chromium_accessibility_tree.json",
 );
 
-describe.todo(ChromiumAccessibilityTree, () => {
+describe(ChromiumAccessibilityTree, () => {
   describe(ChromiumAccessibilityTree.prototype.elementById, () => {
     it("returns correct element for given ID", async () => {
-      const json = await Bun.file(FIXTURE_PATH).text();
+      const json = await Bun.file(FIXTURE_PATH).json();
       const tree = new ChromiumAccessibilityTree(json);
-      expect(tree.elementById(1).backend_node_id).toBe(7);
-      expect(tree.elementById(2).backend_node_id).toBe(6);
-      expect(tree.elementById(3).backend_node_id).toBe(5);
+      expect(tree.elementById(1).backendNodeId).toBe(7);
+      expect(tree.elementById(2).backendNodeId).toBe(6);
+      expect(tree.elementById(3).backendNodeId).toBe(5);
     });
   });
 });
