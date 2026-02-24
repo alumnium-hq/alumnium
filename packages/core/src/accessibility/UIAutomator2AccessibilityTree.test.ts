ac0bb73691d36145b05086ae8e860e72c4a65592
Sasha Koss <koss@nocorp.me>
2026-02-24 12:20:55 +0800

    Fix incorrectly translated UIAutomator2AccessibilityTree test

--- packages/core/src/accessibility/UIAutomator2AccessibilityTree.test.ts
+++ packages/core/src/accessibility/UIAutomator2AccessibilityTree.test.ts
@@ -7,14 +7,14 @@ const FIXTURE_PATH = path.resolve(
   "__fixtures__/uiautomator2_accessibility_tree.xml",
 );
 
-describe.todo(UIAutomator2AccessibilityTree, () => {
+describe(UIAutomator2AccessibilityTree, () => {
   describe(UIAutomator2AccessibilityTree.prototype.elementById, () => {
     it("returns correct element for given ID", async () => {
       const xml = await Bun.file(FIXTURE_PATH).text();
       const tree = new UIAutomator2AccessibilityTree(xml);
       expect(tree.elementById(9)).toMatchObject({
         id: 9,
-        name: "org.wikipedia.alpha:id/fragment_container",
+        androidResourceId: "org.wikipedia.alpha:id/fragment_container",
         type: "android.widget.FrameLayout",
       });
     });
