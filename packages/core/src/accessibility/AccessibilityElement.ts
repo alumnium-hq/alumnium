1de97662b60e5d215b0be2c77159f60a57f1ffce
Sasha Koss <koss@nocorp.me>
2026-02-24 14:32:16 +0800

    Rebuild AccessibilityElement as interface

--- packages/core/src/accessibility/AccessibilityElement.ts
+++ packages/core/src/accessibility/AccessibilityElement.ts
@@ -1,4 +1,4 @@
-export class AccessibilityElement {
+export interface AccessibilityElement {
   id?: number | undefined;
   backendNodeId?: number | undefined;
   name?: string | undefined;
@@ -13,36 +13,4 @@ export class AccessibilityElement {
   frame?: object | undefined;
   locatorInfo?: Record<string, unknown> | undefined;
   frameChain?: number[] | undefined; // For Selenium: chain of iframe backendNodeIds from root to element's frame
-
-  constructor(
-    id?: number,
-    name?: string,
-    label?: string,
-    type?: string,
-    value?: string,
-    backendNodeId?: number,
-    androidResourceId?: string,
-    androidClass?: string,
-    androidText?: string,
-    androidContentDesc?: string,
-    androidBounds?: string,
-    frame?: object,
-    locatorInfo?: Record<string, unknown>,
-    frameChain?: number[],
-  ) {
-    this.id = id;
-    this.backendNodeId = backendNodeId;
-    this.name = name;
-    this.label = label;
-    this.type = type;
-    this.value = value;
-    this.androidResourceId = androidResourceId;
-    this.androidClass = androidClass;
-    this.androidText = androidText;
-    this.androidContentDesc = androidContentDesc;
-    this.androidBounds = androidBounds;
-    this.frame = frame;
-    this.locatorInfo = locatorInfo;
-    this.frameChain = frameChain;
-  }
 }
--- packages/core/src/accessibility/index.ts
+++ packages/core/src/accessibility/index.ts
@@ -1,5 +1,5 @@
-export { AccessibilityElement } from "./AccessibilityElement.js";
-export { BaseAccessibilityTree } from "./BaseAccessibilityTree.js";
-export { ChromiumAccessibilityTree } from "./ChromiumAccessibilityTree.js";
-export { UIAutomator2AccessibilityTree } from "./UIAutomator2AccessibilityTree.js";
-export { XCUITestAccessibilityTree } from "./XCUITestAccessibilityTree.js";
+export * from "./AccessibilityElement.js";
+export * from "./BaseAccessibilityTree.js";
+export * from "./ChromiumAccessibilityTree.js";
+export * from "./UIAutomator2AccessibilityTree.js";
+export * from "./XCUITestAccessibilityTree.js";
