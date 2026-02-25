6d81f639c166df09fa811fe4f58db0f6e2147e5f
Alex Rodionov <p0deje@gmail.com>
2026-02-25 13:51:41 -0800

    feat: lowercase booleans in a11y tree

--- packages/python/tests/server/accessibility/test_server_chromium_accessibilty_tree.py
+++ packages/python/tests/server/accessibility/test_server_chromium_accessibilty_tree.py
@@ -21,50 +21,50 @@ def test_to_xml(chromium_tree: ServerChromiumAccessibilityTree):
     assert (
         chromium_tree.to_xml()
         == """
-<RootWebArea name="TodoMVC: React" id="1" focusable="True">
+<RootWebArea name="TodoMVC: React" id="1" focusable="true">
   <generic id="4">
     <generic id="5">
       <heading id="6" level="1">todos</heading>
       <generic id="8">
-        <textbox name="New Todo Input" id="9" invalid="false" focusable="True" editable="plaintext" settable="True" multiline="False" readonly="False" required="False" labelledby="" />
+        <textbox name="New Todo Input" id="9" invalid="false" focusable="true" editable="plaintext" settable="true" multiline="false" readonly="false" required="false" labelledby="" />
         <LabelText id="12">New Todo Input</LabelText>
       </generic>
     </generic>
     <main id="14">
       <generic id="15">
-        <checkbox id="16" invalid="false" focusable="True" checked="false" />
+        <checkbox id="16" invalid="false" focusable="true" checked="false" />
         <LabelText id="17">Toggle All Input<generic id="18">\\u276f</generic>
         </LabelText>
       </generic>
       <list id="21">
         <listitem id="22" level="1">
-          <checkbox id="24" invalid="false" focusable="True" focused="True" checked="true" />
+          <checkbox id="24" invalid="false" focusable="true" focused="true" checked="true" />
           <LabelText id="25">hello</LabelText>
         </listitem>
         <listitem id="27" level="1">
-          <checkbox id="29" invalid="false" focusable="True" checked="false" />
+          <checkbox id="29" invalid="false" focusable="true" checked="false" />
           <LabelText id="30">he</LabelText>
         </listitem>
       </list>
     </main>
     <generic id="32">1 item left!<list id="35">
         <listitem id="36" level="1">
-          <link id="37" focusable="True">All</link>
+          <link id="37" focusable="true">All</link>
         </listitem>
         <listitem id="39" level="1">
-          <link id="40" focusable="True">Active</link>
+          <link id="40" focusable="true">Active</link>
         </listitem>
         <listitem id="42" level="1">
-          <link id="43" focusable="True">Completed</link>
+          <link id="43" focusable="true">Completed</link>
         </listitem>
       </list>
-      <button id="45" invalid="false" focusable="True">Clear completed</button>
+      <button id="45" invalid="false" focusable="true">Clear completed</button>
     </generic>
   </generic>
   <contentinfo id="47">
     <paragraph id="48">Double-click to edit a todo</paragraph>
     <paragraph id="50">Created by the TodoMVC Team</paragraph>
-    <paragraph id="52">Part of<link id="54" focusable="True">TodoMVC</link>
+    <paragraph id="52">Part of<link id="54" focusable="true">TodoMVC</link>
     </paragraph>
   </contentinfo>
 </RootWebArea>
