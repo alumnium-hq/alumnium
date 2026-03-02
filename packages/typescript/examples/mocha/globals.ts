469c88208cf6c99f10a382192e33161ec5ef26f5
Sasha Koss <koss@nocorp.me>
2026-03-03 07:30:38 +0800

    Fix typescript package TS setup

--- packages/typescript/examples/mocha/globals.ts
+++ packages/typescript/examples/mocha/globals.ts
@@ -1,8 +1,5 @@
 import { type Alumni } from "alumnium";
-import { type WebDriver } from "selenium-webdriver";
-import { type Browser } from "webdriverio";
 
 declare global {
   var al: Alumni;
-  var driver: WebDriver | Browser;
 }
