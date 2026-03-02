d4ca974b971f35adaf38eefd16bf4bb7cfdbe01e
Sasha Koss <koss@nocorp.me>
2026-03-02 14:02:59 +0800

    chore(tsify): make Playwright driver work

--- packages/core/src/client/Alumni.ts
+++ packages/core/src/client/Alumni.ts
@@ -1,6 +1,6 @@
 import type { BaseChatModel } from "@langchain/core/language_models/chat_models";
 import { always } from "alwaysly";
-import { Page } from "playwright";
+import type { Page } from "playwright-core";
 import { WebDriver } from "selenium-webdriver";
 import type { Browser } from "webdriverio";
 import { HttpClient } from "../clients/HttpClient.js";
