9faa445b1c591c495bbe2eae122acd7f8e558d95
Alex Rodionov <p0deje@gmail.com>
2026-04-13 10:18:05 -0700

    fix(ts): embed playwright/selenium files as assets

---
+++ packages/typescript/src/standalone/embeddedAssetNames.ts
@@ -0,0 +1,10 @@
+export const SELENIUM_ATOM_ASSET_PREFIX = "selenium-atom-";
+
+export const PLAYWRIGHT_CORE_PACKAGE_JSON_ASSET_NAME =
+  "playwright-core-package.json";
+
+export const SELENIUM_MANAGER_ASSET_NAMES = {
+  linux: "selenium-manager-linux",
+  macos: "selenium-manager-macos",
+  windows: "selenium-manager-windows.exe",
+} as const;
