635c3e5037b8ce2de4a4a2e3665857dc8d319401
Sasha Koss <koss@nocorp.me>
2026-04-15 20:19:24 +0800

    refactor: add args support to CLI command parser

--- packages/typescript/src/cli/CliCommand.ts
+++ packages/typescript/src/cli/CliCommand.ts
@@ -5,81 +5,150 @@ import { z } from "zod";
 import type { $ZodError, $ZodErrorTree } from "zod/v4/core";
 
 export namespace CliCommand {
-  export interface DefineProps<ArgsSchema extends z.ZodObject> {
+  export interface DefineProps<
+    ArgsSchema extends z.ZodTuple | undefined,
+    OptionsSchema extends z.ZodObject,
+  > {
     name: string;
     description: string;
-    Args: ArgsSchema;
-    action: NoInfer<ActionFn<z.infer<ArgsSchema>>>;
+    Args?: ArgsSchema;
+    Options: OptionsSchema;
+    action: NoInfer<
+      ActionFn<
+        ArgsSchema extends undefined ? undefined : z.infer<ArgsSchema>,
+        z.infer<OptionsSchema>
+      >
+    >;
   }
 
-  export interface Definition<Args> {
+  export type ActionFn<Args, Options> = (
+    props: ActionProps<Args, Options>,
+  ) => Promise<void>;
+
+  export interface ActionProps<Args, Options> {
+    args: Args;
+    options: Options;
+    logFilenameHint: string;
+  }
+
+  export interface Definition<Args, Options> {
     name: string;
     description: string;
-    action: ActionFn<Args>;
+    action: DefinitionActionFn<Args, Options>;
     register: (cli: CAC) => void;
     cli?: CAC | undefined;
   }
 
-  export type ActionFn<Args> = (props: ActionProps<Args>) => Promise<void>;
-
-  export interface ActionProps<Args> {
+  export interface DefinitionActionInput<Args, Options> {
     args: Args;
-    logFilenameHint: string;
+    options: Options;
   }
+
+  export type DefinitionActionFn<Args, Options> = (
+    input: DefinitionActionInput<Args, Options>,
+  ) => Promise<void>;
 }
 
 export abstract class CliCommand {
-  static define<ArgsSchema extends z.ZodObject>(
-    props: CliCommand.DefineProps<ArgsSchema>,
-  ): CliCommand.Definition<z.infer<ArgsSchema>> {
-    const { name, description, Args, action } = props;
+  static define<
+    ArgsSchema extends z.ZodTuple | undefined,
+    OptionsSchema extends z.ZodObject,
+  >(
+    props: CliCommand.DefineProps<ArgsSchema, OptionsSchema>,
+  ): CliCommand.Definition<
+    ArgsSchema extends undefined ? undefined : z.infer<ArgsSchema>,
+    z.infer<OptionsSchema>
+  > {
+    const { name, description, Args, Options, action } = props;
 
-    const definition: CliCommand.Definition<z.infer<ArgsSchema>> = {
+    const definition: CliCommand.Definition<
+      ArgsSchema extends undefined ? undefined : z.infer<ArgsSchema>,
+      z.infer<OptionsSchema>
+    > = {
       name,
       description,
 
-      action: async (rawArgs) => {
-        const argsResult = Args.safeParse(rawArgs);
+      action: async (input) => {
+        const argsResult = Args?.safeParse(input.args);
+        const optionsResult = Options.safeParse(input.options);
 
-        if (argsResult.error) {
-          const { error } = argsResult;
-          printError(error, Args);
-
+        function printHelpAndExit(): never {
           if (definition.cli) {
             console.log(`${ansi.blue("Help:")}\n`);
             definition.cli.outputHelp();
           }
-
           process.exit(1);
         }
 
-        const args = argsResult.data;
+        if (Args && argsResult?.error) {
+          const { error } = argsResult;
+          printVarsError(error, Args, "arguments");
+          printHelpAndExit();
+        }
+
+        if (optionsResult.error) {
+          const { error } = optionsResult;
+          printVarsError(error, Options, "options");
+          printHelpAndExit();
+        }
+
+        const args = argsResult?.data as ArgsSchema extends undefined
+          ? undefined
+          : z.TypeOf<ArgsSchema>;
+        const options = optionsResult.data;
         const logFilenameHint = logFilenameHintFor(name);
-        await action({ args, logFilenameHint });
+        await action({ args, options, logFilenameHint });
       },
 
       register: (cli: CAC) => {
         definition.cli = cli;
-        let command = cli.command(name, description);
 
-        Object.values(Args.shape).forEach((Arg) => {
-          const meta = CliCommand.option.get(Arg);
-          always(meta);
+        const argsSyntax =
+          Args?.def.items?.map((Arg, index) => {
+            const meta = CliCommand.arg.get(Arg);
+            if (!meta)
+              throw new Error(
+                `Missing metadata for argument #${index} in command '${name}'`,
+              );
+            always(meta);
+
+            return meta.syntax;
+          }) || [];
+
+        const commandSyntax = [name].concat(argsSyntax).join(" ");
+        let command = cli.command(commandSyntax, description, {});
+
+        Object.entries(Options.shape).forEach(([key, Option]) => {
+          const meta = CliCommand.option.get(Option);
+          if (!meta)
+            throw new Error(
+              `Missing metadata for option '${key}' in command '${name}'`,
+            );
 
           let defaultValue: string | undefined;
-          if (Arg instanceof z.ZodDefault)
-            defaultValue = String(Arg.def.defaultValue);
+          if (Option instanceof z.ZodDefault)
+            defaultValue = String(Option.def.defaultValue);
 
           command = command.option(meta.syntax, meta.description, {
             default: defaultValue,
           });
         });
 
-        command.action(definition.action);
+        command.action((...argsWithOptions) => {
+          const args: any = Args ? argsWithOptions.slice(0, -1) : undefined;
+          const options = argsWithOptions[argsWithOptions.length - 1];
+          return definition.action({ args, options });
+        });
       },
     };
     return definition;
   }
+
+  static arg = z.registry<{
+    name: string;
+    syntax: string;
+    description: string;
+  }>();
 
   static option = z.registry<{
     name: string;
@@ -93,14 +162,24 @@ function logFilenameHintFor(commandName: string): string {
   return `${commandName}-${logTimeStr}.log`;
 }
 
-function printError<Type>(error: $ZodError<Type>, Schema: z.ZodType<Type>) {
+namespace printVarsError {
+  export type Component = "options" | "arguments";
+}
+
+function printVarsError<Type>(
+  error: $ZodError<Type>,
+  Schema: z.ZodType<Type>,
+  component: printVarsError.Component,
+) {
   const tree = z.treeifyError(error);
 
   const errors: string[] = [];
   printTree(tree, Schema);
 
   if (errors.length) {
-    console.log(`${ansi.red(`Invalid arguments:`)}\n\n${errors.join("\n")}\n`);
+    console.log(
+      `${ansi.red(`Invalid ${component}:`)}\n\n${errors.join("\n")}\n`,
+    );
   }
 
   function printTree<InnerType>(
