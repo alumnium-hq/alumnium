1432836e354d96ef687df2cbe7b7203366dcc2d3
Alex Rodionov <p0deje@gmail.com>
2026-05-19 22:44:28 -0600

    refactor(java): remove unused frame gathering
    Ports e2ccdfb to Java client

---
+++ packages/java/src/test/java/ai/alumnium/system/FramesTest.java
@@ -0,0 +1,32 @@
+package ai.alumnium.system;
+
+import java.io.File;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FramesTest extends BaseTest {
+
+  private static final String CROSS_ORIGIN_IFRAME_URL =
+      new File("../python/examples/support/pages/cross_origin_iframe.html").toURI().toString();
+
+  @Test
+  void testNestedFrames() {
+    navigate("https://the-internet.herokuapp.com/nested_frames");
+
+    al.act("click MIDDLE text");
+    Assertions.assertEquals(
+        List.of("LEFT", "MIDDLE", "RIGHT", "BOTTOM"), al.get("text from all frames"));
+  }
+
+  @Test
+  void testCrossOriginIframe() {
+    navigate(CROSS_ORIGIN_IFRAME_URL);
+
+    al.check("button 'Main Page Button' is present");
+    al.act("click button 'Click Me Inside Iframe'");
+    al.check("text 'Button Clicked!' is present");
+    al.act("click link 'Iframe Link'");
+    al.check("text 'Link Clicked!' is present");
+  }
+}
