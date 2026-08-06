f43425e7429fc0c4ffbd14d8c30ef179eeae2637
Sasha Koss <koss@nocorp.me>
2026-08-06 11:10:34 +0800

    chore(trees): simplify XmlRenderer by removing redundant options

--- packages/typescript/src/xml/XmlRenderer.test.ts
+++ packages/typescript/src/xml/XmlRenderer.test.ts
@@ -1,4 +1,3 @@
-import { type CheerioOptions, load } from "cheerio";
 import { describe, expect, it } from "vitest";
 import { Xml } from "./Xml.ts";
 import { XmlRenderer } from "./XmlRenderer.ts";
@@ -21,21 +20,18 @@ describe("XmlRenderer", () => {
 
     expect(XmlRenderer.render([root], { indentation: "\t" }))
       .toMatchInlineSnapshot(`
-      "<root>
-      \t<child/>
-      </root>"
-    `);
+        "<root>
+        \t<child/>
+        </root>"
+      `);
   });
 
-  it("renders compact XML when minified", () => {
+  it("removes formatting whitespace when minified", () => {
     const root = Xml.element("root", {}, [Xml.element("child")]);
-    expect(
-      XmlRenderer.render([root], {
-        minify: true,
-        xmlMode: true,
-        selfClosingTags: true,
-      }),
-    ).toMatchInlineSnapshot(`"<root><child/></root>"`);
+
+    expect(XmlRenderer.render([root], { minify: true })).toMatchInlineSnapshot(
+      `"<root><child/></root>"`,
+    );
   });
 
   it("sanitizes raw text and attributes", () => {
@@ -48,6 +44,14 @@ describe("XmlRenderer", () => {
         &lt;&lt; Back &amp; done
       </root>"
     `);
+  });
+
+  it("renders empty attributes explicitly", () => {
+    const root = Xml.element("root", { empty: "" });
+
+    expect(XmlRenderer.render([root])).toMatchInlineSnapshot(
+      `"<root empty=""/>"`,
+    );
   });
 
   it("preserves content under xml:space preserve", () => {
@@ -69,305 +73,24 @@ describe("XmlRenderer", () => {
 
     expect(XmlRenderer.render(elements as Xml.AnyElement[]))
       .toMatchInlineSnapshot(`
-      "<?target value?>
-      <one>
-        <!-- note -->
-        <![CDATA[ content ]]>
-      </one>
-      <two/>"
-    `);
+        "<?target value?>
+        <one>
+          <!-- note -->
+          <![CDATA[ content ]]>
+        </one>
+        <two/>"
+      `);
   });
 
-  describe("dom-serializer upstream tests", () => {
-    describe("render DOM parsed with htmlparser2", () => {
-      describe("(html)", () => {
-        const htmlFunction = html.bind(null, { _useHtmlParser2: true });
+  it("minifies multiple roots and special nodes", () => {
+    const elements = Xml.parseRootChildren(
+      "<?target value?><one><!-- note --><![CDATA[ content ]]></one><two/>",
+    );
 
-        it("should handle double quotes within single quoted attributes properly", () => {
-          const markup = "<hr class='an \"edge\" case' />";
-          expect(htmlFunction(markup)).toStrictEqual(
-            '<hr class="an &quot;edge&quot; case">',
-          );
-        });
-
-        it("should escape entities to utf8 if requested", () => {
-          const markup = '<a href="a < b &quot; & c">& " &lt; &gt;</a>';
-          expect(
-            html({ _useHtmlParser2: true, encodeEntities: "utf8" }, markup),
-          ).toStrictEqual(
-            '<a href="a < b &quot; &amp; c">&amp; " &lt; &gt;</a>',
-          );
-        });
-
-        it("should stringify non-string attribute values before escaping", () => {
-          const options: LoadingOptions = { _useHtmlParser2: true };
-          const $ = load("<div></div>", options, true);
-          const div = $("div")[0]!;
-          (div.attribs as Record<string, unknown>).width = 42;
-
-          expect(XmlRenderer.render($._root, { minify: true })).toStrictEqual(
-            '<div width="42"></div>',
-          );
-        });
-      });
-
-      describe(
-        "(html, {})",
-        testBody.bind(null, html.bind(null, { _useHtmlParser2: true })),
-      );
-
-      describe(
-        "(html, {decodeEntities: false})",
-        testBody.bind(
-          null,
-          html.bind(null, { _useHtmlParser2: true, decodeEntities: false }),
-        ),
-      );
-
-      describe("(xml)", () => {
-        it("should render CDATA correctly", () => {
-          const markup =
-            "<a> <b> <![CDATA[ asdf&asdf ]]> <c/> <![CDATA[ asdf&asdf ]]> </b> </a>";
-          expect(xml(markup)).toStrictEqual(markup);
-        });
-
-        it('should append ="" to attributes with no value', () => {
-          expect(xml("<div dropdown-toggle>")).toStrictEqual(
-            '<div dropdown-toggle=""/>',
-          );
-        });
-
-        it('should append ="" to boolean attributes with no value', () => {
-          expect(xml("<input disabled>")).toStrictEqual('<input disabled=""/>');
-        });
-
-        it("should preserve XML prefixes on attributes", () => {
-          const markup =
-            '<div xmlns:ex="http://example.com/ns"><p ex:ample="attribute">text</p></div>';
-          expect(xml(markup)).toStrictEqual(markup);
-        });
-
-        it("should preserve mixed-case XML elements and attributes", () => {
-          const markup = '<svg viewBox="0 0 8 8"><radialGradient/></svg>';
-          expect(xml(markup)).toStrictEqual(markup);
-        });
-
-        it("should encode entities in otherwise special tags", () => {
-          expect(xml('<script>"<br/>"</script>')).toStrictEqual(
-            "<script>&quot;<br/>&quot;</script>",
-          );
-        });
-
-        it("should not encode entities if disabled", () => {
-          const markup = '<script>"<br/>"</script>';
-          expect(xml(markup, { decodeEntities: false })).toStrictEqual(markup);
-        });
-
-        it("should stringify non-string SVG attribute values before escaping", () => {
-          const $ = load("<svg><rect/></svg>", { xmlMode: true }, true);
-          const rect = $("rect")[0]!;
-          (rect.attribs as Record<string, unknown>).width = 42;
-          (rect.attribs as Record<string, unknown>).height = 24;
-
-          expect(
-            XmlRenderer.render($._root, { xmlMode: true, minify: true }),
-          ).toStrictEqual('<svg><rect width="42" height="24"/></svg>');
-        });
-      });
-    });
-
-    describe("(xml, {selfClosingTags: false})", () => {
-      it("should render childless nodes with an explicit closing tag", () => {
-        expect(
-          xml("<foo /><bar></bar>", { selfClosingTags: false }),
-        ).toStrictEqual("<foo></foo><bar></bar>");
-      });
-    });
-
-    describe("(html, {selfClosingTags: true})", () => {
-      it("should render <br /> tags correctly", () => {
-        const markup = "<br />";
-        expect(
-          html(
-            {
-              _useHtmlParser2: true,
-              decodeEntities: false,
-              selfClosingTags: true,
-            },
-            markup,
-          ),
-        ).toStrictEqual(markup);
-      });
-    });
-
-    describe("(html, {selfClosingTags: false})", () => {
-      it("should render childless SVG nodes with an explicit closing tag", () => {
-        const markup =
-          '<svg><circle x="12" y="12"></circle><path d="123M"></path><polygon points="60,20 100,40 100,80 60,100 20,80 20,40"></polygon></svg>';
-        expect(
-          html(
-            {
-              _useHtmlParser2: true,
-              decodeEntities: false,
-              selfClosingTags: false,
-            },
-            markup,
-          ),
-        ).toStrictEqual(markup);
-      });
-    });
-
-    function testBody(
-      htmlFunction: (input: string, options?: LoadingOptions) => string,
-    ): void {
-      it("should render <br /> tags without a slash", () => {
-        expect(htmlFunction("<br />")).toStrictEqual("<br>");
-      });
-
-      it("should retain encoded HTML content within attributes", () => {
-        const markup = '<hr class="cheerio &amp; node = happy parsing" />';
-        expect(htmlFunction(markup)).toStrictEqual(
-          '<hr class="cheerio &amp; node = happy parsing">',
-        );
-      });
-
-      it('should shorten the "checked" attribute when it contains the value "checked"', () => {
-        expect(htmlFunction("<input checked/>")).toStrictEqual(
-          "<input checked>",
-        );
-      });
-
-      it("should render empty attributes if asked for", () => {
-        expect(
-          htmlFunction("<input checked/>", { emptyAttrs: true }),
-        ).toStrictEqual('<input checked="">');
-      });
-
-      it('should not shorten the "name" attribute when it contains the value "name"', () => {
-        expect(htmlFunction('<input name="name"/>')).toStrictEqual(
-          '<input name="name">',
-        );
-      });
-
-      it('should not append ="" to attributes with no value', () => {
-        expect(htmlFunction("<div dropdown-toggle>")).toStrictEqual(
-          "<div dropdown-toggle></div>",
-        );
-      });
-
-      it("should render comments correctly", () => {
-        expect(htmlFunction("<!-- comment -->")).toStrictEqual(
-          "<!-- comment -->",
-        );
-      });
-
-      it("should render whitespace by default", () => {
-        const markup =
-          '<a href="./haha.html">hi</a> <a href="./blah.html">blah</a>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should preserve multiple hyphens in data attributes", () => {
-        const markup = '<div data-foo-bar-baz="value"></div>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should not encode characters in script tag", () => {
-        const markup = '<script>alert("hello world")</script>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should not encode tags in script tag", () => {
-        const markup = '<script>"<br>"</script>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should not encode json data", () => {
-        const markup =
-          '<script>var json = {"simple_value": "value", "value_with_tokens": "&quot;here & \'there\'&quot;"};</script>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should render childless SVG nodes with a closing slash in HTML mode", () => {
-        const markup =
-          '<svg><circle x="12" y="12"/><path d="123M"/><polygon points="60,20 100,40 100,80 60,100 20,80 20,40"/></svg>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should render childless MathML nodes with a closing slash in HTML mode", () => {
-        const markup = "<math><infinity/></math>";
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should allow SVG elements to have children", () => {
-        const markup =
-          '<svg><circle cx="12" r="12"><title>dot</title></circle></svg>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should not include extra whitespace in SVG self-closed elements", () => {
-        const markup = '<svg><image href="x.png"/>     </svg>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should fix-up bad nesting in SVG in HTML mode", () => {
-        expect(
-          htmlFunction('<svg><g><image href="x.png"></svg>'),
-        ).toStrictEqual('<svg><g><image href="x.png"/></g></svg>');
-      });
-
-      it("should preserve XML prefixed attributes on inline SVG nodes in HTML mode", () => {
-        const markup =
-          '<svg><text id="t" xml:lang="fr">Bonjour</text><use xlink:href="#t"/></svg>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should handle mixed-case SVG content in HTML mode", () => {
-        const markup = '<svg viewBox="0 0 8 8"><radialGradient/></svg>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should render HTML content in SVG foreignObject in HTML mode", () => {
-        const markup =
-          '<svg><foreignObject requiredFeatures=""><img src="test.png" viewbox>text<svg viewBox="0 0 8 8"><circle r="3"/></svg></foreignObject></svg>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should render iframe nodes with a closing tag in HTML mode", () => {
-        const markup = '<iframe src="test"></iframe>';
-        expect(htmlFunction(markup)).toStrictEqual(markup);
-      });
-
-      it("should encode double quotes in attribute", () => {
-        const markup = `<img src="/" alt='title" onerror="alert(1)" label="x'>`;
-        expect(htmlFunction(markup)).toStrictEqual(
-          '<img src="/" alt="title&quot; onerror=&quot;alert(1)&quot; label=&quot;x">',
-        );
-      });
-    }
-
-    interface LoadingOptions extends CheerioOptions {
-      _useHtmlParser2?: boolean;
-      decodeEntities?: boolean;
-      encodeEntities?: "utf8";
-      selfClosingTags?: boolean;
-      emptyAttrs?: boolean;
-    }
-
-    function html(
-      preset: LoadingOptions,
-      markup: string,
-      options: LoadingOptions = {},
-    ): string {
-      const mergedOptions = { ...preset, ...options };
-      const $ = load(markup, mergedOptions, true);
-      return XmlRenderer.render($._root, { ...mergedOptions, minify: true });
-    }
-
-    function xml(markup: string, options: LoadingOptions = {}): string {
-      const mergedOptions = { ...options, xmlMode: true };
-      const $ = load(markup, mergedOptions, true);
-      return XmlRenderer.render($._root, { ...mergedOptions, minify: true });
-    }
+    expect(
+      XmlRenderer.render(elements as Xml.AnyElement[], { minify: true }),
+    ).toMatchInlineSnapshot(
+      `"<?target value?><one><!-- note --><![CDATA[ content ]]></one><two/>"`,
+    );
   });
 });
--- packages/typescript/src/xml/XmlRenderer.ts
+++ packages/typescript/src/xml/XmlRenderer.ts
@@ -6,8 +6,7 @@
  */
 
 import * as ElementType from "domelementtype";
-import { Element, isTag, isText, Text } from "domhandler";
-import { encodeXML, escapeAttribute, escapeText } from "entities";
+import { Element, Text } from "domhandler";
 import type {
   AnyNode,
   CDATA,
@@ -19,203 +18,17 @@ export namespace XmlRenderer {
   export interface Options {
     minify?: boolean;
     indentation?: string;
-    emptyAttrs?: boolean;
-    selfClosingTags?: boolean;
-    xmlMode?: boolean | "foreign";
-    encodeEntities?: boolean | "utf8";
-    decodeEntities?: boolean;
   }
 }
 
 export abstract class XmlRenderer {
-  static #unencodedElements = new Set([
-    "style",
-    "script",
-    "xmp",
-    "iframe",
-    "noembed",
-    "noframes",
-    "plaintext",
-    "noscript",
-  ]);
-
-  static #voidElements = new Set([
-    "area",
-    "base",
-    "basefont",
-    "br",
-    "col",
-    "command",
-    "embed",
-    "frame",
-    "hr",
-    "img",
-    "input",
-    "isindex",
-    "keygen",
-    "link",
-    "meta",
-    "param",
-    "source",
-    "track",
-    "wbr",
-  ]);
-
-  static #foreignElements = new Set(["svg", "math"]);
-
-  static #foreignModeIntegrationPoints = new Set([
-    "mi",
-    "mo",
-    "mn",
-    "ms",
-    "mtext",
-    "annotation-xml",
-    "foreignObject",
-    "desc",
-    "title",
-  ]);
-
-  static #elementNames = new Map(
-    [
-      "altGlyph",
-      "altGlyphDef",
-      "altGlyphItem",
-      "animateColor",
-      "animateMotion",
-      "animateTransform",
-      "clipPath",
-      "feBlend",
-      "feColorMatrix",
-      "feComponentTransfer",
-      "feComposite",
-      "feConvolveMatrix",
-      "feDiffuseLighting",
-      "feDisplacementMap",
-      "feDistantLight",
-      "feDropShadow",
-      "feFlood",
-      "feFuncA",
-      "feFuncB",
-      "feFuncG",
-      "feFuncR",
-      "feGaussianBlur",
-      "feImage",
-      "feMerge",
-      "feMergeNode",
-      "feMorphology",
-      "feOffset",
-      "fePointLight",
-      "feSpecularLighting",
-      "feSpotLight",
-      "feTile",
-      "feTurbulence",
-      "foreignObject",
-      "glyphRef",
-      "linearGradient",
-      "radialGradient",
-      "textPath",
-    ].map((name) => [name.toLowerCase(), name]),
-  );
-
-  static #attributeNames = new Map(
-    [
-      "definitionURL",
-      "attributeName",
-      "attributeType",
-      "baseFrequency",
-      "baseProfile",
-      "calcMode",
-      "clipPathUnits",
-      "diffuseConstant",
-      "edgeMode",
-      "filterUnits",
-      "glyphRef",
-      "gradientTransform",
-      "gradientUnits",
-      "kernelMatrix",
-      "kernelUnitLength",
-      "keyPoints",
-      "keySplines",
-      "keyTimes",
-      "lengthAdjust",
-      "limitingConeAngle",
-      "markerHeight",
-      "markerUnits",
-      "markerWidth",
-      "maskContentUnits",
-      "maskUnits",
-      "numOctaves",
-      "pathLength",
-      "patternContentUnits",
-      "patternTransform",
-      "patternUnits",
-      "pointsAtX",
-      "pointsAtY",
-      "pointsAtZ",
-      "preserveAlpha",
-      "preserveAspectRatio",
-      "primitiveUnits",
-      "refX",
-      "refY",
-      "repeatCount",
-      "repeatDur",
-      "requiredExtensions",
-      "requiredFeatures",
-      "specularConstant",
-      "specularExponent",
-      "spreadMethod",
-      "startOffset",
-      "stdDeviation",
-      "stitchTiles",
-      "surfaceScale",
-      "systemLanguage",
-      "tableValues",
-      "targetX",
-      "targetY",
-      "textLength",
-      "viewBox",
-      "viewTarget",
-      "xChannelSelector",
-      "yChannelSelector",
-      "zoomAndPan",
-    ].map((name) => [name.toLowerCase(), name]),
-  );
-
   static render(
     node: AnyNode | ArrayLike<AnyNode>,
     options: XmlRenderer.Options = {},
   ): string {
-    if (options.minify) return this.#renderCompact(node, options);
-
     const nodes = "length" in node ? node : [node];
-    for (let index = 0; index < nodes.length; index++) {
-      this.#sanitizeNode(nodes[index]!);
-    }
-
-    const rendererOptions = {
-      xmlMode: true,
-      encodeEntities: false,
-      emptyAttrs: true,
-      selfClosingTags: true,
-      ...options,
-    } satisfies XmlRenderer.Options;
-
-    return this.#renderFormattedChildren(nodes, rendererOptions, 0, false);
-  }
-
-  static #renderCompact(
-    node: AnyNode | ArrayLike<AnyNode>,
-    options: XmlRenderer.Options,
-  ): string {
-    const nodes = "length" in node ? node : [node];
-    const xmlMode = options.xmlMode ?? false;
-    let output = "";
-
-    for (let index = 0; index < nodes.length; index++) {
-      output += this.#renderNode(nodes[index]!, options, xmlMode);
-    }
-
-    return output;
+    const output = this.#renderFormattedChildren(nodes, options, 0, false);
+    return options.minify ? output.replace(/\n\s*/g, "") : output;
   }
 
   static #renderFormattedChildren(
@@ -234,7 +47,7 @@ export abstract class XmlRenderer {
           data += (children[++index] as Text).data;
         }
 
-        const trimmed = data.trim();
+        const trimmed = this.#sanitizeText(data.trim());
         if (!trimmed) continue;
         if (output) output += "\n";
         output += `${this.#indent(depth, options)}${trimmed}`;
@@ -271,7 +84,9 @@ export abstract class XmlRenderer {
     }
 
     if (node.type === ElementType.Text) {
-      const data = preserveSpace ? node.data : node.data.trim();
+      const data = this.#sanitizeText(
+        preserveSpace ? node.data : node.data.trim(),
+      );
       if (!data) return "";
       return `${preserveSpace ? "" : this.#indent(depth, options)}${data}`;
     }
@@ -295,11 +110,14 @@ export abstract class XmlRenderer {
       return `${preserveSpace ? "" : this.#indent(depth, options)}<${directive.data}${suffix}>`;
     }
 
-    return `${preserveSpace ? "" : this.#indent(depth, options)}${this.#renderNode(
-      node,
-      options,
-      true,
-    )}`;
+    const prefix = preserveSpace ? "" : this.#indent(depth, options);
+    if (node.type === ElementType.Comment) {
+      return `${prefix}<!--${(node as Comment).data}-->`;
+    }
+    if (node.type === ElementType.CDATA) {
+      return `${prefix}<![CDATA[${((node as CDATA).children[0] as Text).data}]]>`;
+    }
+    return "";
   }
 
   static #renderFormattedTag(
@@ -310,7 +128,7 @@ export abstract class XmlRenderer {
   ): string {
     const name = element.name;
     const prefix = preserveSpace ? "" : this.#indent(depth, options);
-    const attributes = this.#formatAttributes(element.attribs, options, true);
+    const attributes = this.#formatAttributes(element.attribs);
     const children = element.children;
 
     if (!children.length) return `${prefix}<${name}${attributes}/>`;
@@ -334,137 +152,19 @@ export abstract class XmlRenderer {
     return `${prefix}<${name}${attributes}>\n${renderedChildren}\n${this.#indent(depth, options)}</${name}>`;
   }
 
-  static #renderChildren(
-    children: ArrayLike<AnyNode>,
-    options: XmlRenderer.Options,
-    xmlMode: boolean | "foreign",
-  ): string {
-    let output = "";
-    for (let index = 0; index < children.length; index++) {
-      output += this.#renderNode(children[index]!, options, xmlMode);
-    }
-    return output;
-  }
-
-  static #renderNode(
-    node: AnyNode,
-    options: XmlRenderer.Options,
-    xmlMode: boolean | "foreign",
-  ): string {
-    switch (node.type) {
-      case ElementType.Root:
-        return this.#renderChildren(node.children, options, xmlMode);
-      case ElementType.Directive:
-        return `<${(node as ProcessingInstruction).data}>`;
-      case ElementType.Comment:
-        return `<!--${(node as Comment).data}-->`;
-      case ElementType.CDATA:
-        return `<![CDATA[${((node as CDATA).children[0] as Text).data}]]>`;
-      case ElementType.Script:
-      case ElementType.Style:
-      case ElementType.Tag:
-        return this.#renderTag(node as Element, options, xmlMode);
-      case ElementType.Text: {
-        const element = node as Text;
-        const data = element.data || "";
-        if (
-          (options.encodeEntities ?? options.decodeEntities) !== false &&
-          !(
-            !xmlMode &&
-            element.parent &&
-            this.#unencodedElements.has((element.parent as Element).name)
-          )
-        ) {
-          return (
-            xmlMode || options.encodeEntities !== "utf8"
-              ? encodeXML
-              : escapeText
-          )(data);
-        }
-        return data;
-      }
-    }
-  }
-
-  static #renderTag(
-    element: Element,
-    options: XmlRenderer.Options,
-    xmlMode: boolean | "foreign",
-  ): string {
-    if (xmlMode === "foreign") {
-      element.name = this.#elementNames.get(element.name) ?? element.name;
-      if (
-        element.parent &&
-        this.#foreignModeIntegrationPoints.has((element.parent as Element).name)
-      ) {
-        xmlMode = false;
-      }
-    }
-
-    if (!xmlMode && this.#foreignElements.has(element.name)) {
-      xmlMode = "foreign";
-    }
-
-    const { name, children } = element;
-    const isVoid = !xmlMode && this.#voidElements.has(name);
-    let tag = `<${name}${this.#formatAttributes(element.attribs, options, xmlMode)}`;
-
-    if (
-      children.length === 0 &&
-      (xmlMode
-        ? options.selfClosingTags !== false
-        : options.selfClosingTags && isVoid)
-    ) {
-      tag += xmlMode ? "/>" : " />";
-    } else {
-      tag += ">";
-      if (children.length) {
-        tag += this.#renderChildren(children, options, xmlMode);
-      }
-      if (!isVoid) tag += `</${name}>`;
-    }
-
-    return tag;
-  }
-
   static #formatAttributes(
     attributes: Record<string, unknown> | undefined,
-    options: XmlRenderer.Options,
-    xmlMode: boolean | "foreign",
   ): string {
     if (!attributes) return "";
 
-    const encode =
-      (options.encodeEntities ?? options.decodeEntities) === false
-        ? this.#replaceQuotes
-        : xmlMode || options.encodeEntities !== "utf8"
-          ? encodeXML
-          : escapeAttribute;
-    const isForeign = xmlMode === "foreign";
-    const isShowEmpty = !!(options.emptyAttrs ?? xmlMode);
     let result = "";
 
     for (const key in attributes) {
       const value = attributes[key];
-      const name = isForeign ? (this.#attributeNames.get(key) ?? key) : key;
-      result +=
-        !isShowEmpty && (value == null || value === "")
-          ? ` ${name}`
-          : ` ${name}="${encode(value == null ? "" : String(value))}"`;
+      result += ` ${key}="${this.#sanitizeAttr(value == null ? "" : String(value))}"`;
     }
 
     return result;
-  }
-
-  static #sanitizeNode(node: AnyNode): void {
-    if (isText(node)) {
-      node.data = this.#sanitizeText(node.data);
-    } else if (isTag(node)) {
-      for (const [name, value] of Object.entries(node.attribs)) {
-        node.attribs[name] = this.#sanitizeAttr(value);
-      }
-      for (const child of node.children) this.#sanitizeNode(child);
-    }
   }
 
   static #sanitizeText(value: string): string {
@@ -488,10 +188,6 @@ export abstract class XmlRenderer {
         return `&#x${character.charCodeAt(0).toString(16).toUpperCase()};`;
       },
     );
-  }
-
-  static #replaceQuotes(value: string): string {
-    return value.replaceAll('"', "&quot;");
   }
 
   static #indent(depth: number, options: XmlRenderer.Options): string {
