9260dad89161798c863e9366d9b7c0da62ec5070
Sasha Koss <koss@nocorp.me>
2026-08-07 07:56:02 +0800

    chore(website): switch to production settings

--- websites/docs/src/pages/robots.txt.ts
+++ websites/docs/src/pages/robots.txt.ts
@@ -2,5 +2,7 @@ import type { APIRoute } from "astro";
 
 export const GET: APIRoute = () =>
   new Response(`User-agent: *
-Disallow: /
+Allow: /
+
+Sitemap: https://alumnium.ai/sitemap-index.xml
 `);
